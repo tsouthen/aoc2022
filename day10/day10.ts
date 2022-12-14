ce0dfa15e30aeb8e56acdeb8add6ad809409c34c
Todd Southen <tsouthen@gmail.com>
2022-12-13 23:11:32 -0800

    Refined code to use reduce, chunk and map

--- day10/day10.ts
+++ day10/day10.ts
@@ -1,35 +1,32 @@
 import { readFile } from "../utils/utils.ts";
+import { chunk } from "https://deno.land/std@0.167.0/collections/chunk.ts";
 
 const lines = readFile("input.txt", import.meta.url);
 
 const values = new Array<number>();
-values.push(0); //to deal with "end of cycle" processing
 lines.forEach((line) => {
   if (line.length === 0) return;
-  if (line === "noop") {
-    values.push(0);
-  } else if (line.startsWith("addx ")) {
-    values.push(0);
+  values.push(0);
+  if (line.startsWith("addx "))
     values.push(Number(line.substring(5)));
-  }
 });
 
-let sum = 1;
-let signalStrength = 0;
-for (let num = 20; num < values.length; num += 40) {
-  sum = values.slice(Math.max(0, num - 40), num).reduce((acc, curr) => acc + curr, sum);
-  signalStrength += (num * sum);
-}
+values.unshift(0); //to deal with "end of cycle" processing
+const { signalStrength } = values.reduce(({sum, signalStrength}, value, index) => {
+  return { 
+    sum: sum + value, 
+    signalStrength: signalStrength + ((((index - 20) % 40) === 0) ? index * sum : 0),
+  };
+}, { sum: 1, signalStrength: 0 });
+values.shift(); //remove the extra 0 added
 console.log(`Part 1: ${signalStrength}`);
 
 console.log("Part 2:");
-values.shift(); //remove the extra 0 added in Part 1 to deal with "end of cycle" processing
-let pos = 1;
-while (values.length) {
-  const line = values.splice(0, 40).reduce((acc, val, index) => {
-    const pixel = (Math.abs(pos - index) <= 1) ? "#" : ".";
-    pos += val;
-    return acc + pixel;
-  }, "");
-  console.log(line);
-}
+let position = 1;
+const pixels = values.map((value, index) => {
+  const result = Math.abs(position - (index % 40)) <= 1 ? "#" : " ";
+  position += value;
+  return result;
+});
+const result = chunk(pixels, 40).map(line => line.join("")).join("\n");
+console.log(result);
