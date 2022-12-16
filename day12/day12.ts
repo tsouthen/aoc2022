2eeebc5ee76b88c46131220fb0b3a7a2eaf4654c
Todd Southen <tsouthen@gmail.com>
2022-12-16 10:14:27 -0800

    Day 12 completed

--- day12/day12.ts
+++ day12/day12.ts
@@ -12,7 +12,7 @@ class Node {
   get height() {
     const aVal = "a".charCodeAt(0);
     if (this.value === "S")
-      return aVal;
+      return 0;
     if (this.value === "E")
       return "z".charCodeAt(0) - aVal;
     return this.value.charCodeAt(0) - aVal;
@@ -23,7 +23,6 @@ class Node {
   }
 }
 
-const lines = readFile("sample.txt", import.meta.url);
 const map = new Matrix<Node>();
 let id = 0;
 
@@ -45,21 +44,20 @@ const options : AStarOptions = {
   neighbor: (n: Node) => {
     const neighbours = new Array<Node>();
     const pt = n.getCoordinate(map);
-    // gather all neighbouring nodes that have the same height or lower
     const width = map[0].length;
     const height = map.length;
     for (let x = pt.x - 1; x <= pt.x + 1; x += 2) {
       if (x >= 0 && x < width) {
-        const neighbour = map[x][pt.y];
+        const neighbour = map[pt.y][x];
         if (n.canMoveTo(neighbour))
-          neighbours.push(n);
+          neighbours.push(neighbour);
       }
     }
     for (let y = pt.y - 1; y <= pt.y + 1; y += 2) {
       if (y >= 0 && y < height) {
-        const neighbour = map[pt.x][y];
+        const neighbour = map[y][pt.x];
         if (n.canMoveTo(neighbour))
-          neighbours.push();
+          neighbours.push(neighbour);
       }
     }
     return neighbours;
@@ -78,6 +76,8 @@ const options : AStarOptions = {
     return n.idx.toString();
   }
 };
+
+const lines = readFile("input.txt", import.meta.url);
 map.load(lines, (char => {
   const node = new Node(id++, char);
   if (char === "S") 
@@ -86,6 +86,29 @@ map.load(lines, (char => {
     options.end = node;
   return node;
 }));
-console.log(`${map.length}`);
-const result = aStar(options);
-console.log(result);
+
+function part1() {
+  const result = aStar(options);
+  if (result.status === "success")
+    console.log(`Part 1: ${result.path.length - 1}`);
+  else
+    console.log(`Part 1: failed`);  
+}
+
+function part2() {
+  let minDistance = 0;
+  map.traverse((n => {
+    if (n.height === 0) {
+      options.start = n;
+      const result = aStar(options);
+      if (result.status === "success") {
+        const currDist = result.path.length - 1;
+        minDistance = minDistance === 0 ? currDist : Math.min(minDistance, currDist);
+      }
+    }
+  }));
+  console.log(`Part 2: ${minDistance}`);
+}
+
+part1();
+part2();
