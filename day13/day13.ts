f71569cecf256fdd26c0c120e2c5ea727c2bcdbc
Todd Southen <tsouthen@gmail.com>
2022-12-16 17:33:58 -0800

    Code cleanup and part 2

--- day13/day13.ts
+++ day13/day13.ts
@@ -1,66 +1,71 @@
 import { readFile } from "../utils/utils.ts";
 import { chunk } from "https://deno.land/std@0.167.0/collections/chunk.ts";
 
-type Packet = number | Array<Packet>;
+type Packet = Array<number | Packet>;
 
 enum Order {
-  Correct, Incorrect, Continue
+  Correct = -1, 
+  Continue = 0,
+  Incorrect = 1 
 }
 
-function correctlyOrdered(left: Packet, right: Packet): Order {
-  const lType = typeof left;
-  const rType = typeof right;
+function compareNumbers(left: number, right: number) {
+  if (left < right) return Order.Correct;
+  if (left === right) return Order.Continue;
+  return Order.Incorrect;
+}
 
-  if (lType === "number" && rType === "number") { //both are numbers
-    return left < right ? Order.Correct : (left === right ? Order.Continue : Order.Incorrect);
-  }
-  if (lType === "number") { //right is array
-    return correctlyOrdered([left], right);
-  }
-  if (rType === "number") { //left is array
-    return correctlyOrdered(left, [right]);
-  }
-  //ensure they're arrays
-  if (!Array.isArray(left) || !Array.isArray(right)) {
-    debugger;
-    return Order.Incorrect; //throw instead?
-  }
+function isNumber(packet: number | Packet): packet is number {
+  return typeof packet === "number";
+}
 
-  //both sides are arrays
-  if (left.length === 0) {
-    return Order.Correct;
-  }
-  if (right.length === 0) {
-    return Order.Incorrect;
-  }
-  // both have data
-  for (let ii = 0; ii < left.length; ii++) {
-    if (ii === right.length)
-      return Order.Incorrect;
-    const order = correctlyOrdered(left[ii], right[ii]);
-    if (order != Order.Continue)
+function possiblyToArray(packetVal: number | Packet) {
+  if (isNumber(packetVal))
+    return [packetVal];
+  return packetVal;
+}
+
+function comparePackets(left: Packet, right: Packet): Order {
+  for (let ii = 0; ii < Math.min(left.length, right.length); ii++) {
+    const [lVal, rVal] = [left[ii], right[ii]];
+    let order = Order.Continue;
+    if (isNumber(lVal) && isNumber(rVal))
+      order = compareNumbers(lVal, rVal);
+    else
+      order = comparePackets(possiblyToArray(lVal), possiblyToArray(rVal));
+
+    if (order !== Order.Continue)
       return order;
   }
-  return left.length < right.length ? Order.Correct : Order.Continue;
+  return compareNumbers(left.length, right.length);
 }
 
 const lines = readFile("input.txt", import.meta.url);
+const packets = lines.filter((l) => l.length).map(l => JSON.parse(l));
 
 function part1() {
   let idx = 1;
   let result = 0;
-  chunk(lines, 3).forEach(([left, right]) => {
-    const [leftData, rightData] = [JSON.parse(left), JSON.parse(right)];
-    const order = correctlyOrdered(leftData, rightData);
-    if (order === Order.Continue) {
-      debugger;
-    } else if (order === Order.Correct) {
+  chunk(packets, 2).forEach(([leftData, rightData]) => {
+    if (comparePackets(leftData, rightData) === Order.Correct)
       result += idx;
-      console.log(`${idx}`);
-    }
     idx++;
   });
-  // console.log(`Part 1: ${result}`);
+  console.log(`Part 1: ${result}`);
 }
 
+function findDivider(packets: Array<Packet>, divider: number) {
+  return packets.findIndex((p) => {
+    return p.length === 1 && (typeof p[0] === "object") && p[0].length === 1 && p[0][0] === divider;
+  });
+}
+
+function part2() {
+  const packets2 = new Array<Packet>(...packets, [[2]], [[6]]);
+  packets2.sort(comparePackets);
+  const twoIdx = 1 + findDivider(packets2, 2);
+  const sixIdx = 1 + findDivider(packets2, 6);
+  console.log(`Part 2: ${twoIdx * sixIdx}`);
+}
 part1();
+part2();
